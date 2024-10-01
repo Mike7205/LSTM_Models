3b0a450a2167129a424645bf2d0d4731ff8d0d5e
Michał Leśniewski <141821434+Mike7205@users.noreply.github.com>
2024-10-01 14:51:42 +0200

    Update LSTM_new_com11.py

--- LSTM_new_com11.py
+++ LSTM_new_com11.py
@@ -71,7 +71,7 @@ st.subheader('AI about the markets', divider='red')
 from transformers import pipeline
 
 # Inicjalizacja modelu
-generator = pipeline('text-generation', model='distilgpt2')
+generator = pipeline('text-generation', model='gpt2')
 # Pole tekstowe do wprowadzenia promptu
 prompt = st.text_input("How about:", "Latest insides")
 
