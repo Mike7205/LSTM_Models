1832e08711a972367fe98437336d597688e95160
Michał Leśniewski <141821434+Mike7205@users.noreply.github.com>
2024-10-02 11:17:01 +0200

    Add files via upload

--- LSTM_new_com11.py
+++ LSTM_new_com11.py
@@ -131,4 +131,5 @@ with st.form("my_form"):
     # Every form must have a submit button.
     submitted = st.sidebar.form_submit_button("Submit")
     if submitted:
-        #st.write("slider", slider_val, "checkbox", checkbox_val)
+        
+#st.write("slider", slider_val, "checkbox", checkbox_val)
